74ce787ec387b3cb8fd51b0844736f25cda3b321
Sungurova_Svetlanta <sungurovasveta@yandex.ru>
2022-12-28 20:46:29 +0300

    add

--- bjs/08_if_else/script.js
+++ bjs/08_if_else/script.js
@@ -36,6 +36,25 @@ document.getElementById('btnOver').addEventListener('click', function () {
         }
     }
 })
+document.getElementById('btnLess').addEventListener('click', function () {
+    if (gameRun){
+        if (maxValue === minValue){
+            const phraseRandom = Math.round( Math.random());
+            const answerPhrase = (phraseRandom === 1) ?
+                `Вы загадали неправильное число!\n\u{1F914}` :
+                `Я сдаюсь..\n\u{1F92F}`;
+
+            answerField.innerText = answerPhrase;
+            gameRun = false;
+        } else {
+            maxValue = answerNumber  +1;
+            answerNumber  = Math.floor((minValue + maxValue) / 2);
+            orderNumber++;
+            orderNumberField.innerText = orderNumber;
+            answerField.innerText = `Вы загадали число ${answerNumber }?`;
+        }
+    }
+})
 
 document.getElementById('btnEqual').addEventListener('click', function () {
     if (gameRun){
@@ -43,4 +62,6 @@ document.getElementById('btnEqual').addEventListener('click', function () {
         gameRun = false;
     }
 })
+document.getElementById('btRetry').addEventListener('click', function () {
 
+})
